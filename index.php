c7ea8bce910f0360b67dfbd707c2658d15dc355e
EmiliyaHrabova <emiliya.hrabova@gmail.com>
2020-11-20 00:38:16 +0100

    php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+<body>
+	<h1>HOLA! </h1>
+	<p>just whatever </p>
+	
+</body>
+</html>
+
+<a href="dash.php" title="Open Dash">Open Dash</a>
+
+<?php
+	$_SESSION["loggedIn"] = "false";
+?>
+
